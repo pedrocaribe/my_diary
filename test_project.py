455c398cf3c9deda8d25bc86976149a1f6500891
Pedro Caribe <pho.caribe@gmail.com>
2023-08-20 23:40:14 -0300

    Implementation of test_project

--- test_project.py
+++ test_project.py
@@ -1,4 +1,6 @@
 # import pytest
+import os.path
+
 from project import main, setup_login_window, validate_login, create_login
 import sqlite3
 from tkinter import *
@@ -37,5 +39,23 @@ def test_create_login():
     # Call function and check if successfully accepts arguments
     create_login(db, l_back_window, b_reg)
 
+    # Check if db was successfully created
+    assert os.path.exists("diary.db") is True
 
+    # Check if db is unlocked
+    db = sqlite3.connect("diary.db")
 
+    assert db is not None
+
+    # Check if db was created with correct table
+    cursor = db.execute("SELECT sql FROM sqlite_master WHERE name = ?", ["accounts"])
+    schema = cursor.fetchone()[0]
+
+    assert schema == ("CREATE TABLE accounts ("
+                      "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                      "account TEXT, "
+                      "first_name TEXT, "
+                      "last_name TEXT, "
+                      "hashed_password BLOB, "
+                      "key_verification BLOB)"
+                      )
